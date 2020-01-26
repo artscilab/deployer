525927e1b14808727be06b384efbac9e5c1b9655
Al Madireddy <al.madireddy@gmail.com>
2020-01-25 18:21:14 -0600

    git deploy working

--- config.js
+++ config.js
@@ -2,15 +2,15 @@
 const projects = {
   "creativedisturbance": {
     dev: {
-      deployPath: "/var/www/dev.creativedisturbance.org/html/wp_content/themes/creativedisturbance"
+      deployPath: "/var/www/dev.creativedisturbance.org/html/wp-content/themes/creativedisturbance"
     },
     master: {
-      deployPath: "/var/www/creativedisturbance.org/html/wp_content/themes/creativedisturbance"
+      deployPath: "/var/www/creativedisturbance.org/html/wp-content/themes/creativedisturbance"
     }
   },
   "cdash": {
     master: {
-      deployPath: "/var/www/cdash.atec.io/html/wp_content/themes/cdash"
+      deployPath: "/var/www/cdash.atec.io/html/wp-content/themes/cdash"
     }
   },
   "artscilab": {
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express")
 const node_ssh = require("node-ssh")
 const body_parser = require("body-parser")
@@ -16,9 +17,13 @@ app.get("/", (req, res) => {
 })
 
 app.post("/webhook", async (req, res) => {
-  const { ref, repository: { name } } = req.body;
+  const { ref, repository: { name, clone_url} } = req.body;
   const configuration = require("./config")[name]
 
+  // load configuration from config.js and check that 
+  // a config exists for this repository. If the repository 
+  // name is 'artscilab' then the 'artscilab' key must be present
+  // at the top level of the configuration object. 
   if (!configuration) {
     res.status(404).json({
       "message": `configuration for "${name}" not found`
@@ -33,6 +38,11 @@ app.post("/webhook", async (req, res) => {
     return;
   }
   
+  // Get the branch that this push came from and check that options
+  // for that branchname exists in the configuration for this repo.
+  // If the repo is 'artscilab' and we want to deploy on push to the 
+  // 'master' branch, then the 'master' key must exist in 
+  // the configuration object for 'artscilab' 
   const branchName = ref.split("/").pop();
   const branchConfig = configuration[branchName];
   if (!branchConfig) {
@@ -42,6 +52,9 @@ app.post("/webhook", async (req, res) => {
     return
   }
   
+  // The configuration for a branch must include the 'deployPath' 
+  // key, which should specify the folder in which the code 
+  // will be cloned into. 
   const { deployPath } = branchConfig;
   if (!deployPath) {
     res.status(200).json({
@@ -51,10 +64,44 @@ app.post("/webhook", async (req, res) => {
   }
 
   const ssh = new node_ssh();
-  // TODO: connect to server and clone the repo. 
+  // connect to the server using the SSH client and private key 
+  // from the environment variable. 
+  try {
+    await ssh.connect({
+      host: "atec.io",
+      username: "git",
+      privateKey: process.env.SSH_KEY
+    });
+  } catch (e) {
+    console.log(`Error on connect: ${e}`);
+    res.status(500).json({
+      "message": "couldn't connect to atec.io"
+    })
+    return
+  }
+
+  // run the "git pull" command to bring in the latest changes
+  // meaning the deployPath must already be a github repo
+  // note: in the future this will be a configurable
+  // option with mode:git or mode:file, the latter of which 
+  // will manually upload the files from the repo to the deploy path,
+  // which allows the user to not depend on git locally on the server. 
+  try {
+    const cloneCommand = `git pull`;
+    console.log(`running ${cloneCommand} in ${deployPath}`);
+    await ssh.execCommand(cloneCommand, {
+      cwd: deployPath
+    })
+  } catch(e) {
+    console.log(`Error on deploy: ${e}`)
+    res.status(500).json({
+      "message": "error running git pull"
+    })
+    return 
+  }
 
   res.json({
-    "message": "success"
+    "message": `successfully deployed ${name}:${branchName} to ${deployPath}`
   })
 })
 
